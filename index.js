5f91b8a32de82a77f7e87aa0de0b7045006ada99
Brian Hines <brian@projectweekend.net>
2015-02-09 07:02:39 -0600

    Hook up in index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.authentication = require( "./lib/authentication" );
 exports.blitzio = require( "./lib/blitzio" );
 exports.database = require( "./lib/database" );
+exports.errors = require( "./lib/errors" );
