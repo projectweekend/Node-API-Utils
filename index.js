86d98fe893aa03c0e50162f68f4a9d26618b9ae9
Brian Hines <brian@projectweekend.net>
2015-03-04 06:07:29 -0600

    Add response callbacks

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.authentication = require( "./lib/authentication" );
 exports.blitzio = require( "./lib/blitzio" );
 exports.database = require( "./lib/database" );
 exports.errors = require( "./lib/errors" );
+exports.responses = require( "./lib/responses" );
