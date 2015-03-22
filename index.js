dca9196caffebcce30d5eadcf0daca021620b646
Brian Hines <brian@projectweekend.net>
2015-03-22 07:49:16 -0500

    Testing a create handler idea

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ exports.database = require( "./lib/database" );
 exports.errors = require( "./lib/errors" );
 exports.responses = require( "./lib/responses" );
 exports.cache = require( "./lib/cache" );
+exports.CreateHandler = require( "./lib/handlers/create" );
